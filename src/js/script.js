bfb3fd239cbd856f3098b29b95e1d6706b6241af
malindu22 <malindu.gimhan18@gmail.com>
2022-08-06 11:49:10 +0530

    panorama add

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,32 @@
-import * as THREE from 'three';
-import * as PANOLENS from 'panolens-three';
+const panorama = new PANOLENS.ImagePanorama( './assets/img.jpg' );
+let viewer = new PANOLENS.Viewer({
+    autoHideControlBar: true,
+    autoHideInfospot: false, 
+    output: 'console',
+    dwellTime: 1000
+});
 
-const renderer = new THREE.WebGL1Renderer();
+const infospot = new PANOLENS.Infospot(300, './assets/small.png');
+infospot.position.set( 5000, 203, -636 );
+infospot.addHoverText( 'Out Side', 30 );
+panorama.add( infospot );
 
-renderer.setSize(window.innerWidth,window.innerHeight);
+const infospot2 = new PANOLENS.Infospot();
+infospot2.position.set( 5000, -263, 1236 );
+infospot2.addHoverText( 'Next', 30 );
+panorama.add( infospot2 );
 
-document.body.appendChild(renderer.domElement); 
+viewer.add( panorama );
+
+infospot2.addEventListener('click',() => {
+    infospot2.unlockHoverElement()
+    viewer.remove( panorama );
+    const newpanorama = new PANOLENS.ImagePanorama( './assets/img1.jpg' );
+    viewer.add( newpanorama );
+    viewer.setPanorama( newpanorama );
+});
+
+document.addEventListener('click',() => {
+    let clickedPos = viewer.getPosition();
+    console.log("data", clickedPos)
+});
